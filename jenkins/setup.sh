9de48ecee52177b4d65dd876f22f30faece19df2
Aditya Misra <amisra14@yahoo.co.uk>
2019-04-15 17:56:36 +0530

    Update setup.sh

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -4,7 +4,7 @@ docker run -p 8080:8080 \
 --name=jenkins-master -d \
 --env JAVA_OPTS="-Xmx1500m" \
 --env JENKINS_OPTS=" --handlerCountMax=300 --logfile=/var/log/jenkins/jenkins.log" \
--v /Users/aditya.misra/moneyview/hackathon/jenkins-data:/var/jenkins_home \
--v /Users/aditya.misra/moneyview/hackathon/jenkins:/var/log/jenkins \
+-v /home/jenkins-data:/var/jenkins_home \
+-v /home/jenkins:/var/log/jenkins \
 --privileged \
 jenkins
